ff6e0a6e638e8985f73f8260bdae7b75bd5c7aec
Daniel Viray <124648222+VirDan21@users.noreply.github.com>
2023-05-23 05:15:43 +0800

    Add files via upload

---
+++ DDD.sql
@@ -0,0 +1,3 @@
+SELECT * FROM ddd.users;
+insert into users(email_add,fname,position,contact_number,office,local_line)
+values('viraydaniel21@gmail.com','Daniel Joseph M. Viray','President','9278842723','CMO','863');
